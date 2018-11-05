097a73c2ae3cf4cf35fe9e6f80182654c6e3aab9
unknown <mikrut.3@osu.edu>
2018-11-04 19:06:34 -0500

    updated stn

--- src/telematics/lib/CarT/src/CAN.h
+++ src/telematics/lib/CarT/src/CAN.h
@@ -7,13 +7,8 @@ class CAN
     public:
         CAN();
         void begin();
-<<<<<<< HEAD
         void monitorOBD();
         int receive(int* buffer, int& size);
-=======
-        void transmit();
-        char receive();
->>>>>>> 06338b0451a1fed0ad6113047f5c32284c2fcdfc
     private:
 };
 #endif
