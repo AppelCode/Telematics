e09438ad8ff4b4c6fa8201dc0faba94816632ee4
AppelCode <appel.60@osu.edu>
2018-11-04 19:20:18 -0500

    update

--- src/telematics/lib/CarT/src/CAN.cpp
+++ src/telematics/lib/CarT/src/CAN.cpp
@@ -5,18 +5,52 @@ CAN::CAN() {
     //initialization parameters
 }
 
-void CAN::transmit() 
-{
+void CAN::monitorOBD() {
     Serial5.write("stma\r");
 }
 
-void CAN::begin()
-{
+void CAN::begin(){
     Serial5.begin(9600); 
     while(!Serial5);      
     Serial5.write("atz\r");
     delay(1200);
 }
+
+int CAN::receive(int* buffer, int& size){
+    int temp;
+    int i = 0;
+
+    while(Serial5.available())
+    {
+        temp = Serial5.read();
+        *(buffer+i) = temp;
+        i++;
+    }
+    size = i;
+    return Serial5.available();
+}
+
+/*
+char CAN::receive(){
+    char rxData[32];
+    char rxIndex = 0;
+    char n = 0;
+    while(n != '\r'){
+        if (Serial5.available() > 0){
+                if (Serial5.peak() == '\r'){
+                    n = Serial5.read();
+                    rxData[rxIndex] = '\0';
+                    rxIndex = 0;
+                }
+        }
+        else {
+            n = Serial5.read();
+            rxData[rxIndex++] = n;
+        }
+    }
+    return rxData;      
+ }
+ */
 
 
 
