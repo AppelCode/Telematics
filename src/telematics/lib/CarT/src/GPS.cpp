3e673174ec263ef562087e0a7f31849d23275f90
Shawnbad <43895352+Shawnbad@users.noreply.github.com>
2018-11-19 12:05:38 -0500

    GPS wrappers

---
+++ src/telematics/lib/CarT/src/GPS.cpp
@@ -0,0 +1,51 @@
+#include "GPS.h"
+
+GPS::GPS(){
+	//constructor
+}
+
+
+void GPS::begin()
+{
+  gps.begin(9600);
+  gps.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  delay(1000);
+  gps.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  pinMode(D6, OUTPUT);
+  digitalWrite(D6, LOW);
+}
+
+bool GPS::flag(){
+  bool condition;
+  (gps.fix) ? condition = TRUE : condition = FALSE;
+  return condition; 
+}
+
+void GPS::getCoord()
+{
+    while (GPSSerial.available()){
+        char c = gps.read();
+        if (gps.newNMEAreceived()){
+            gps.parse(gps.lastNMEA());
+            }
+    }
+	LAT = gps.latitudeDegrees;
+	LON = gps.longitudeDegrees; 	      
+	
+}
+
+void GPS::time(){
+    while (GPSSerial.available()){
+        char c = gps.read();
+        if (gps.newNMEAreceived()){
+            gps.parse(gps.lastNMEA());
+            }
+    }
+    HR = gps.hour;
+    MIN = gps.minute;
+    SEC = gps.seconds;
+    MIL = gps.milliseconds;
+    D = gps.day;
+    M = gps.month;
+    Y = gps.year;
+}
---
+++ src/telematics/lib/CarT/src/GPS.h
@@ -0,0 +1,33 @@
+#ifndef GPS_H
+#define GPS_H
+#include "Adafruit_GPS.h"
+#define GPSSerial Serial1
+
+// This is your main class that users will import into their application
+
+
+class GPS
+{
+public:
+
+  GPS();
+
+  void begin();
+
+  void getCoord();
+  
+  bool flag();
+  
+  void time();
+  
+  float LAT, LON;
+  uint8_t HR, MIN, SEC, Y, M, D;
+  uint16_t MIL;
+  
+
+private:
+
+   Adafruit_GPS gps = Adafruit_GPS(&GPSSerial);
+
+};
+#endif
